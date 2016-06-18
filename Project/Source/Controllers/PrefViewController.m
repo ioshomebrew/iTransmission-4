af69092ccd7f0ccca02c800687c279339c938caf
Beecher Adams <koalaos127@gmail.com>
2016-06-17 21:14:56 -0700

    Fix pref view controller

--- Project/Source/Controllers/PrefViewController.m
+++ Project/Source/Controllers/PrefViewController.m
@@ -131,7 +131,7 @@
 {
     switch (section) {
         case 0: return 2;
-        case 1: return 2;
+        case 1: return 1;
             case 2: return 4;
             case 3: return 2;
             case 4: return 2;
